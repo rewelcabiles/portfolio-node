9572410086e68589c319c91e0f4d24ee9a81d281
Rewel Cabiles <rewel.sicuan@gmail.com>
2022-02-25 07:40:01 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+
+
+// Template
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname+'public/css'));
+
+// Routes
+app.get('/', (req, res) => {  
+    res.render('index');
+
+});
+
+app.listen(5000)
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./public/**/*.{html,js,ejs}", "./views/**/*.{html,js,ejs}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
