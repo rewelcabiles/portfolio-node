2b35b3403d12bd9958fd0e2d54ba822ee2a44a43
Rewel Cabiles <rewel.sicuan@gmail.com>
2022-02-25 13:12:11 +0800

    First iteration of the new website

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
-
+const http = require('http')
 
 // Template
 app.use(expressLayouts);
@@ -10,6 +10,7 @@ app.set('view engine', 'ejs');
 // Static Files
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname+'public/css'));
+
 
 // Routes
 app.get('/', (req, res) => {  
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@ module.exports = {
   content: ["./public/**/*.{html,js,ejs}", "./views/**/*.{html,js,ejs}"],
   theme: {
     extend: {},
+    letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.05em',
+      widest: '.1em',
+      widest: '.25em',
+      widerest: '.75em',
+    }
   },
   plugins: [],
 }
